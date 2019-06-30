f220f8f8c89e67311ba1413494c6914d226c2162
kaldren <drenski666@gmail.com>
2019-06-30 15:31:22 +0300

    Added fields to Customer entity

--- SmallDad.Domain/Entities/Customer.cs
+++ SmallDad.Domain/Entities/Customer.cs
@@ -6,5 +6,12 @@ namespace SmallDad.Domain.Entities
 {
     public class Customer
     {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
     }
 }
